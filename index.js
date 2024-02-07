00025717d3c027369b8a3b95618edfcc48508fe6
Ankit Raj <ankitrajcollab@gmail.com>
2024-02-07 18:10:23 +0000

    basics 1 done

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// variable is like a temprory space which is used to
+// store person data like name , address, ad many more
+// constanta are final variable which value can't change 
+
+const accountId = 144553;
+
+let accEmail = "ankit@gmail.com";
+
+var accountPassword = "12345";
+
+accountBranch = "sasaram";
+
+//----------
+
+// accountId = 34342;
+
+accEmail = "acc23232@mail";
+
+accountPassword = "22scse";
+
+console.log(accountId);
+
+console.log(accEmail);
+
+console.log(accountPassword)
+
+console.table([accEmail,accountId,accountPassword,accountBranch])
