5d38499695618968c5601a36e38df4741890330d
Ankit Raj <ankitrajcollab@gmail.com>
2024-02-07 18:41:34 +0000

    data type and file name changed

---
+++ 02_dataType.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all js code as newer version of jsavaScript 
+
+ // alert(3 + 3) // we are using nodejs , not browser 
+
+// refer mdn for js as well or ecma 
+
+// Data Type In Js 
+
+// String -> 'hello' & "hello" are same 
+
+// Number -> 3 , 34,34  etc
+
+// BigInt -> 900719925124740999n
+
+// boolen -> true / false 
+
+// undefined -> let a; - matlab abhi value assign nahi hue hai
+
+//-----------------------------
+// null -> let a = null;  -- null ek object hai
+// ye ek standaloone value hai null ek representation hai empty value ka
+//-------------------------------------------
+
+// symbol -> let value = symbol('hello') ;c- when we are talking about uniqueness
+
+
+// object -> let student ={};
+
+// Note: The Object data type (non-primitive type) can store collections of data,
+// whereas primitive data type can only store a single data.
+
+
+//-----------
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined 
+
+
+// Assigment 
+
+// ecma script pe jake document padho numbers or object ke bare me 
