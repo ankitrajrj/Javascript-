9798ab2d4b569e7d366da39d1c865fb4d652aabb
Ankit Raj <ankitrajcollab@gmail.com>
2024-02-12 23:03:54 +0000

    conversion and prepost fix

--- 03_conversion.js
+++ 03_conversion.js
@@ -43,3 +43,59 @@ let someNumber = 33;
 let NumberToString = String(someNumber);
 console.log(NumberToString);
 console.log(typeof NumberToString);
+
+
+// ************* Operations ****************
+
+let value = 3;
+
+let negValue = -value;
+
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // two ki power 3
+console.log(2/3);
+console.log(27%9);
+
+
+//----
+
+let str1 = "hello";
+let str2 = " Ankit";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+
+console.log("1" + (2+2)); // 14
+console.log("1" + 2 + 2); // 122 
+console.log(1 + 2 + "2"); // 32
+
+
+// bad practises 
+
+console.log(+true) //1
+console.log(+"")   // 2
+
+let num1 , num2 , num3 ;
+num1 = num2 =  num3 = 2+2;
+
+//-------
+
+// postfix and prefix 
+
+let counter = 100;
+
+console.log(counter++) //it incremnt value after using previous value 
+console.log(++counter) // it incerement value before using value 
+
+
+// go to mDN and read about pre and post fix 
+
+
+
