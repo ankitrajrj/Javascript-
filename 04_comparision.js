d822041b98e3e3cfce23a69784a273412a4d80b6
Ankit Raj <ankitrajcollab@gmail.com>
2024-02-13 13:03:17 +0000

    comparision

---
+++ 04_comparision.js
@@ -0,0 +1,38 @@
+console.log(" ----------------- Basic Conversion ------------------------ ")
+
+console.log(2>1); //true
+console.log(2 >= 1);  //true 
+console.log(2 < 1);   // false
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+
+console.log("--------------- part 1 -----------------")
+
+// js automatically convert other data type into number 
+// for these type of conversion
+
+//  ish tarah ke null or undefinefd wale conversion karne se bache 
+
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+console.log(null > 0); // false
+ 
+console.log(null == 0); // false
+
+console.log(null >= 0); // true - due to value conversion 
+
+// javascript me comparision and equality check alag tarah se kaam krta hai ..
+
+console.log("--------------- part 2 -----------------")
+
+
+console.log(undefined == 0); // false 
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false 
+
+// === tripal equal value ke sath data type ko bhi check krta hai ...
+
+
+
