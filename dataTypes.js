2174c51eefdb8699a52545ffc59914f4c2bca7d0
Ankit Raj <ankitrajcollab@gmail.com>
2024-02-14 23:32:20 +0530

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -1,6 +1,25 @@
 
 
-// kaise deta ko memory me rakha jata hai or store kiya jat 
+// kaise deta ko memory me rakha jata hai or store kiya jata
+// kaise access krte hai  
 // ush basis pr deta 2 catogary hai
 
-// premitive and non premitive
+// 1. premitive  
+
+// sare premitive data type call by value hote hai 
+// inka ek copy banta hai wo apko diya jata hai 
+
+// 7 types - String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+// 2. non premitive
+
+// Array , Object ,  Function 
+// sare non premitive ka return type object hi hota hai  or function ka return type object function hai 
+
+
+
+// javaScript Kaise LAnguage Hai
+
+// JavaScript is a dynamically typed language
+
+
