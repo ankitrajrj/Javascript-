a02801b2bc4aa3da9c8ef4ca962d83d75a1ed598
Ankit Raj <ankitrajcollab@gmail.com>
2024-02-21 21:05:03 +0000

    complete till Function Part One

--- 12_Object2.js
+++ 12_Object2.js
@@ -25,4 +25,15 @@ const regularUser = {
     }
 }
 
-console.log(regularUser);
+console.log(regularUser.full_name.username);
+
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+const obj3 = {obj1,obj2};
+
+const obj4 = {...obj1,...obj2};
+
+console.log(obj3); // ishme 2 object ek sath marge ho jainge 
+console.log(obj4); // ishme ek hi object me sare key values aajainge 
